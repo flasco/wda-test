20914249819a29ab80e19590fd68789409e5a186
czq <chenzhengqiang@youzan.com>
2018-09-25 22:27:43 +0800

    fix: 更新依赖

--- src/app.js
+++ src/app.js
@@ -1,15 +1,29 @@
 
 // const wda = require('wda-driver');
-const wda = require('flasco_wda-driver');
+// const wda = require('flasco_wda-driver');
 
 // com.nhnent.SKQUEST 克鲁赛德战记
 class App {
   async start() {
-    const c = new wda.Client('http://localhost:8100');
+    // const c = new wda.Client('http://localhost:8100');
+    // 0ED53A48-CA24-455A-8093-153CC0D35330
+    // const s = await c.quickSession('0ED53A48-CA24-455A-8093-153CC0D35330');
+    // console.log(await s.orientation());
+    // await c.screenshot('test1.png');
+    // let x = sharp(img).raw().toBuffer((err, data, info) => {
 
-    const s = await c.session('com.nhnent.SKQUEST');
-    console.log(await s.orientation());
-    await s.close();
+    //   const offset = channels * (width * y + x);
+    //   const red = data[offset];
+    //   const green = data[offset + 1];
+    //   const blue = data[offset + 2];
+
+    // });
+
+    // console.log(await s.orientation());
+    // await s.close();
+    // console.log(await s.getWindowSize());
+    // 横屏游戏 零点位于右上角
+    // await s.tap(704, 10); // 关闭按钮
   }
 }
 
