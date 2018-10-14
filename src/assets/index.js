750fc8691e39a416b828d2fb3982ca89c072734e
czq <chenzhengqiang@youzan.com>
2018-10-14 10:10:38 +0800

    fix: 调整图片资源的存放结构

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,8 +1,19 @@
 module.exports = {
-  isStart: `${__dirname}/isStart.png`,
-  isLoading_1: `${__dirname}/isLoading-1.png`,
-  isLoading_2: `${__dirname}/isLoading-2.png`,
-  close_1: `${__dirname}/close-1.png`,
+  isStart: `${__dirname}/common/isStart.png`,
+  isLoading_1: `${__dirname}/common/isLoading-1.png`,
+  close_1: `${__dirname}/common/close-1.png`,
+  yesBtn: `${__dirname}/common/yesBtn.png`,
 
-  isLoadedFirst: `${__dirname}/isLoadedFirst.png`,
+  isAtHome: `${__dirname}/home/isAtHome.png`,
+
+  // 冒险相关
+  adventure: `${__dirname}/home/adventure/adventure.png`,
+
+  // 面包相关
+  bread: `${__dirname}/home/bread/bread.png`,
+
+  // 好友相关
+  friends: `${__dirname}/home/friend/friends.png`,
+  exchange: `${__dirname}/home/friend/exchange.png`,
+  exchange_disabled: `${__dirname}/home/friend/exchange-disabled.png`,
 };
