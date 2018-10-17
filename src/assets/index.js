5716091c3394ac26c57b114f46a0e8480f6a599a
czq <chenzhengqiang@youzan.com>
2018-10-17 22:19:25 +0800

    fix: 调整资源结构

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,19 +1,12 @@
+const homeImgs = require('./home');
+
 module.exports = {
+  // common相关
   isStart: `${__dirname}/common/isStart.png`,
   isLoading_1: `${__dirname}/common/isLoading-1.png`,
   close_1: `${__dirname}/common/close-1.png`,
   yesBtn: `${__dirname}/common/yesBtn.png`,
 
-  isAtHome: `${__dirname}/home/isAtHome.png`,
-
-  // 冒险相关
-  adventure: `${__dirname}/home/adventure/adventure.png`,
-
-  // 面包相关
-  bread: `${__dirname}/home/bread/bread.png`,
-
-  // 好友相关
-  friends: `${__dirname}/home/friend/friends.png`,
-  exchange: `${__dirname}/home/friend/exchange.png`,
-  exchange_disabled: `${__dirname}/home/friend/exchange-disabled.png`,
+  // home相关
+  ...homeImgs,
 };
